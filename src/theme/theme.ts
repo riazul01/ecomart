bc8789912cd2032f82e353ec9bbc698d55657dd9
Riazul Islam <riazul5207@gmail.com>
2024-08-30 20:56:31 +0600

    button base component updated

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,12 +3,14 @@ import palette from './palette';
 import typography from './typography';
 import CssBaseline from './components/utils/CssBaseline';
 import Button from './components/button/Button';
+import ButtonBase from './components/button/ButtonBase';
 
 export const theme = createTheme({
   palette,
   typography,
   components: {
     MuiButton: Button,
+    MuiButtonBase: ButtonBase,
 
     MuiCssBaseline: CssBaseline,
   },
