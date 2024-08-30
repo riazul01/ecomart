3c0b0cf4057015e6988752de9b3f5bc41bb125df
Riazul Islam <riazul5207@gmail.com>
2024-08-30 20:52:01 +0600

    css baseline updated

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,8 +1,12 @@
 import { createTheme } from '@mui/material/styles';
 import palette from './palette';
 import typography from './typography';
+import CssBaseline from './components/utils/CssBaseline';
 
 export const theme = createTheme({
-    palette,
-    typography,
+  palette,
+  typography,
+  components: {
+    MuiCssBaseline: CssBaseline,
+  },
 });
