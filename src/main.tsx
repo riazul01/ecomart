fd47c98f0a7f4ca61b1a10a2f6cd23586b8711b7
Riazul Islam <riazul5207@gmail.com>
2024-08-30 18:12:36 +0600

    theme provider added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import router from 'routes/router';
 import { RouterProvider } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from 'theme/theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
