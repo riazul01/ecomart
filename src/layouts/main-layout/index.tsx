94b4512b07e108f43edb36cdfa8d6d1fce91fa8f
Riazul Islam <riazul5207@gmail.com>
2024-08-31 17:14:21 +0600

    navbar and sidebar section added

--- src/layouts/main-layout/index.tsx
+++ src/layouts/main-layout/index.tsx
@@ -1,14 +1,32 @@
-import { PropsWithChildren } from 'react';
+import { useState, PropsWithChildren } from 'react';
 import Stack from '@mui/material/Stack';
+import Sidebar from './sidebar';
 import Topbar from './topbar';
 import Header from './header';
+import Navbar from './navbar';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
   return (
     <Stack width={1} minHeight="100vh">
+      <Sidebar drawerOpen={drawerOpen} toggleDrawer={toggleDrawer} />
       <Stack component="main" direction="column" width={1}>
         <Topbar />
         <Header />
+        <Navbar toggleDrawer={toggleDrawer} />
         {children}
       </Stack>
     </Stack>
