3b8082f43d4800e6462f37d151afe7a37f8c7b2c
Riazul Islam <riazul5207@gmail.com>
2024-09-27 15:11:00 +0600

    products data updated

---
+++ src/data/products.ts
@@ -0,0 +1,49 @@
+import CabbageImage from 'assets/images/cabbage.png';
+
+export interface ProductData {
+  id: string | number;
+  name: string;
+  weight: number;
+  unit: string;
+  price: number;
+  discountInPercent: number;
+  rating: number;
+  ratingCount: number;
+  image: string;
+  desc: string;
+  category: string;
+  tags: string[];
+  inStock: boolean;
+  stockCount: number;
+  sales: number;
+  brandName: string;
+  brandLink: string;
+}
+
+const product: ProductData = {
+  id: 1,
+  name: 'Chinese cabbage',
+  weight: 1,
+  unit: 'kg',
+  price: 60,
+  discountInPercent: 10,
+  rating: 4.2,
+  ratingCount: 5000,
+  image: CabbageImage,
+  desc: '',
+  category: 'vegetables',
+  tags: ['vegetable', 'green', 'organic'],
+  inStock: true,
+  stockCount: 1200,
+  sales: 124032,
+  brandName: 'freshfirm',
+  brandLink: '',
+};
+
+const products: ProductData[] = [];
+
+for (let i = 0; i <= 24; i++) {
+  products.push(product);
+}
+
+export { products };
