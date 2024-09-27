f7246ea4356f6ad067d2f36dbdc6ae55efa7d0a4
Riazul Islam <riazul5207@gmail.com>
2024-09-27 22:57:46 +0600

    product slider added

--- src/components/sections/home/featured-products/index.tsx
+++ src/components/sections/home/featured-products/index.tsx
@@ -1,22 +1,9 @@
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import ProductSlider from 'components/common/ProductSlider';
 import ProductCard from 'components/common/ProductCard';
+import { products } from 'data/products';
 
 const FeaturedProducts = () => {
-  return (
-    <Box mb={8}>
-      <Stack mb={2.25} justifyContent="space-between" alignItems="center">
-        <Typography variant="h4" fontWeight={600}>
-          Featured Products
-        </Typography>
-      </Stack>
-
-      <Box>
-        <ProductCard />
-      </Box>
-    </Box>
-  );
+  return <ProductSlider title="Featured Products" SliderCard={ProductCard} data={products} />;
 };
 
 export default FeaturedProducts;
