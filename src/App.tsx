8676c3dee6e843d07074c4b53bd6fe5e598209eb
Riazul Islam <riazul5207@gmail.com>
2024-12-18 23:55:59 +0600

    theme updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1 className="text-center text-cyan-500">$$$ Hello World! $$$</h1>;
+  return (
+    <h1 className="text-center text-cyan-500 font-poppins">
+      $$$ Hello World! $$$
+    </h1>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+import { theme } from './src/theme/theme.ts';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme,
   plugins: [],
 }
 
